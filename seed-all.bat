bf2029f03b33d1f623df22a1cf0b36e1739f8156
Van <vanpc1906@gmail.com>
2026-08-27 11:49:41 +0700

    update .gitignore

--- seed-all.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 > nul
-echo ==================================================
-echo 🚀 ECOMMERCE MICROSERVICES - SEED DATA RUNNER
-echo ==================================================
-python "%~dp0src\scripts\seed-all.py"
-pause
